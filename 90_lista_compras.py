c97503b8b8c276744975fd2a0cc01c92771013a0
Daniel Silva Tigre <aniel.tigre19@gmail.com>
2025-01-13 00:59:51 -0300

    Initial commit

---
+++ 90_lista_compras.py
@@ -0,0 +1,21 @@
+import os
+decisão = ''
+lista_decompras = []  
+while True:
+    
+    try:
+        decisão = int(input("\nEscolha uma das seguintes opções:\n[1]Adicionar\n[2]Remover\n[3]Ver lista\n"))
+    except:
+        print("Por favor, insira um valor válido!")
+    if decisão == 1:
+        produto = input("\nInforme o produto que deseja adicionar: \n")
+        lista_decompras.append(produto) 
+    elif decisão ==2:
+        indice_remover = int(input("Informe o Índice que você deseja remover(o índice [0]) representa o primeiro produto: "))
+        lista_decompras.pop(indice_remover)
+    elif decisão == 3: 
+        print("lista de compras: \n")
+        if len(lista_decompras)==0:
+            print(f'lista de compras vazia')
+        for indice, produto in enumerate(lista_decompras):
+            print(indice,produto) 
